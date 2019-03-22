7eaad3c0ad1507804033260d02811cb2a0057215
seanoughton <seanoughton@gmail.com>
2019-03-22 12:45:39 -0700

    added in some error checking for the inputs

--- rock-paper-scissors-procedural.py
+++ rock-paper-scissors-procedural.py
@@ -5,7 +5,10 @@ import random
 
 while True:
     # ask the user for input and assign it to a variable
-    player1 = input('What hand shape do you choose? (rock,paper,scissors)')
+    # add in some error checking
+    player1 = ''
+    while player1 not in ['rock','paper','scissors']:
+        player1 = input('What hand shape do you choose? (rock,paper,scissors)')
     # create the computer player input
     hand_shapes = ['rock','paper','scissors']
     player2 = hand_shapes[random.randint(0,2)]
@@ -32,7 +35,9 @@ while True:
 
     print(result)
     # print out the result (Win,Lose,Draw)
+    play_again = ''
+    while play_again not in ['y','n']:
+        play_again = input('Would you like to play again?')
 
-    play_again = input('Would you like to play again?')
-    if play_again == 'n':
+    if play_again.lower() == 'n':
         break
